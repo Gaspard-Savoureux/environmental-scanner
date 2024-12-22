bff93102f8bd116d705fbd7685d7700e8dcb9cc2
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2024-12-22 16:43:51 -0500

    Refactoring + Added environment test

--- scanner-ui/src/main.rs
+++ scanner-ui/src/main.rs
@@ -1,7 +1,10 @@
+use key_mapping::apply_input;
 use macroquad::prelude::*;
+mod key_mapping;
 
 const MOVING_STEP: f32 = 0.1;
 const TARGET: Vec3 = vec3(0., 0., 0.);
+const DOT_SIZE: f32 = 0.1;
 
 #[macroquad::main("scanner-ui")]
 async fn main() {
@@ -10,7 +13,22 @@ async fn main() {
 
     // Horizontal and vertical angles (in radians):
     let mut yaw: f32 = 0.0;
+    // let mut pitch: f32 = 0.0;
     let mut pitch: f32 = -10.0;
+
+    let mut vec = Vec::new();
+    for i in -10..11 {
+        for j in 0..4 {
+            vec.push(vec3(i as f32, j as f32, 10.));
+            vec.push(vec3(i as f32, j as f32, -10.));
+            vec.push(vec3(10., j as f32, i as f32));
+            vec.push(vec3(-10., j as f32, i as f32));
+        }
+    }
+
+    // Just to show that `vec` is populated:
+    println!("vec length: {}", vec.len());
+    println!("First element: {:?}", vec[0]);
 
     loop {
         clear_background(LIGHTGRAY);
@@ -21,7 +39,6 @@ async fn main() {
 
         let camera_position = vec3(x, y, z);
 
-        // Going 3d!
         set_camera(&Camera3D {
             position: camera_position,
             up: vec3(0., 1., 0.),
@@ -33,30 +50,23 @@ async fn main() {
         if is_key_pressed(KeyCode::Escape) {
             break;
         }
-
-        if is_key_down(KeyCode::Up) {
-            pitch += MOVING_STEP;
-        }
-        if is_key_down(KeyCode::Down) {
-            pitch -= MOVING_STEP;
-        }
-        if is_key_down(KeyCode::Left) {
-            yaw -= MOVING_STEP;
-        }
-        if is_key_down(KeyCode::Right) {
-            yaw += MOVING_STEP;
-        }
+        apply_input(&MOVING_STEP, &mut pitch, &mut yaw);
 
         draw_grid(20, 1., BLACK, GRAY);
 
-        draw_cube_wires(vec3(0., 1., -6.), vec3(2., 2., 2.), DARKGREEN);
-        draw_cube_wires(vec3(0., 1., 6.), vec3(2., 2., 2.), DARKBLUE);
-        draw_cube_wires(vec3(2., 1., 2.), vec3(2., 2., 2.), YELLOW);
+        // draw_cube_wires(vec3(0., 1., -6.), vec3(2., 2., 2.), DARKGREEN);
+        // draw_cube_wires(vec3(0., 1., 6.), vec3(2., 2., 2.), DARKBLUE);
+        // draw_cube_wires(vec3(2., 1., 2.), vec3(2., 2., 2.), YELLOW);
 
-        draw_cube(vec3(0., 0., 0.), vec3(0.2, 0.8, 0.2), None, BLACK);
-        draw_sphere(vec3(0., 1., 1.), 0.1, None, GREEN);
+        // draw_cube(vec3(0., 0., 0.), vec3(0.2, 0.8, 0.2), None, BLACK);
+        // draw_sphere(vec3(1., 2., 3.), 0.1, None, GREEN);
 
-        draw_sphere(vec3(-8., 0., 0.), 1., None, BLUE);
+        // draw_sphere(vec3(-8., 0., 0.), 1., None, BLUE);
+
+        // Test
+        for pos in &vec {
+            draw_cube(*pos, vec3(DOT_SIZE, DOT_SIZE, DOT_SIZE), None, GREEN);
+        }
 
         // Back to screen space, render some text
         set_default_camera();
